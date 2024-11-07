2b38b77f98984a8c9d4e1a430218bfffbc5be2fc
orocko-v2 <101337734+orocko-v2@users.noreply.github.com>
2024-11-07 22:04:19 +0700

    Лаб 2.1

--- lab1_2.py
+++ lab1_2.py
@@ -5,12 +5,12 @@ import info
 
 version = info.version
 
-def doLab2():
+def doLab2(return_type):
     print('lab1.2')
     resp = requests.get('https://api.vk.com/method/groups.getMembers',
                         params={
                             'access_token': info.service_key,
-                            'count': 500,
+                            'count': 50,
                             'group_id': 'tomsk123',
                             'sort': 'id_asc',
                             'v': version
@@ -33,14 +33,14 @@ def doLab2():
                                 'name_case': 'nom',
                                 'v': version,
                             })
-        return_list.append(resp.json()['response'][0])
-        print(resp.json()['response'][0])
         if resp.json()['response'][0]['is_closed'] or 'deactivated' in resp.json()['response'][0].keys():
             member_count -= 1
             continue
+        return_list.append(resp.json()['response'][0])
+        print(resp.json()['response'][0])
         if not resp.json()['response'][0].get('bdate'):
             age = 0
-        elif len(resp.json()['response'][0]['bdate']) != 10:
+        elif len(resp.json()['response'][0]['bdate']) != 9 and len(resp.json()['response'][0]['bdate']) != 10:
             age = 0
         else:
             age = 2024 - int(resp.json()['response'][0]['bdate'][-4:])
@@ -67,6 +67,7 @@ def doLab2():
             else:
                 city_dict['NA'] = 1
 
+    print(len([age for age in age_list if age != 0]))
     average_age = sum(age for age in age_list if age != 0) / len([age for age in age_list if age != 0])
     median_age = median([age for age in age_list if age != 0])
     unknown_age = len([age for age in age_list if age == 0]) / member_count * 100
@@ -85,7 +86,12 @@ def doLab2():
     for key, value in sorted(city_dict.items(), key=lambda x: x[1], reverse=True):
         print("{} : {}".format(key, value))
 
-    return return_list
+    if return_type == 'users':
+        return return_list
+    elif return_type == 'cities':
+        return city_dict
+    else:
+        return (return_list, city_dict)
 
 
 
--- lab2_1.py
+++ lab2_1.py
@@ -6,22 +6,42 @@ import lab1_2
 group_users_graph = Network(notebook=True)
 group_users_graph.show_buttons(filter_=True)
 group_users_graph.repulsion(node_distance=100, spring_length=150)
+color_map = {
+    1: '#FFB6C1',
+    2: '#4169E1'
+}
 
-users = lab1_2.doLab2()
+data = lab1_2.doLab2('all')
+users = data[0]
+cities = data[1]
+del cities['NA']
 
-group_users_graph.add_node(
-    1,
-    label='Group',
-    title = 'Регион-70 | Томск',
-    color = '#1818c7'
+get_colors = lambda n: ["#%06x" % random.randint(0, 0xFFFFFF) for _ in range(n)]
+get_colors(3)
+
+cities_list = list(range(1, len(cities)+1))
+group_users_graph.add_nodes(
+    list(range(1, len(cities)+1)),
+    label=list(cities.keys()),
+    title = list(cities.keys()),
+    color = get_colors(len(cities)),
+    shape = ['box']*len(cities)
 )
 
 group_users_graph.add_nodes(
     [el['id'] for el in users],
-    label = ['User'] * len(users),
+    label = [el['first_name'] for el in users],
     title = [str(el['first_name'] + ' ' + el['last_name']) for el in users],
-    color = ['#%02X%02X%02X' % (random.randint(0,255),random.randint(0,255),random.randint(0,255))]*len(users)
+    color = [color_map[el['sex']] for el in users]
 )
-group_users_graph.add_edges([(1, el['id']) for el in users])
+
+for user in users:
+    for city in cities_list:
+        if user.get('city'):
+            if group_users_graph.get_node(city)['title'] == user['city']['title']:
+                group_users_graph.add_edge(city, user['id'])
+
+for edge in group_users_graph.get_edges():
+    edge['color'] = 'black'
 group_users_graph.show('graph1.html')
 
